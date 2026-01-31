05d440be4a035ba51c1a8bf450cf9468e7c28a03
RenanCatini <renan.amaral@sou.unifal-mg.edu.br>
2026-01-31 11:16:07 -0300

    Primeiro commit do projeto Muralize

---
+++ src/main/java/com/renancatini/Muralize/MuralizeApplication.java
@@ -0,0 +1,13 @@
+package com.renancatini.Muralize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuralizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuralizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renancatini/Muralize/MuralizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renancatini.Muralize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuralizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
